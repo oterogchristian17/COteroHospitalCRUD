ad1e7494689ae70105f38d4d8d623cc54062fc27
JustinoAT <jus.delatorre48@gmail.com>
2024-02-28 14:08:03 -0600

    Initial commit

---
+++ DL/CoteroHospitalContext.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace DL;
+
+public partial class CoteroHospitalContext : DbContext
+{
+    public CoteroHospitalContext()
+    {
+    }
+
+    public CoteroHospitalContext(DbContextOptions<CoteroHospitalContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Especialidad> Especialidads { get; set; }
+
+    public virtual DbSet<Hospital> Hospitals { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=.; Database= COteroHospital; Trusted_Connection=True; User ID=sa; Password=pass@word1; TrustServerCertificate=true;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Especialidad>(entity =>
+        {
+            entity.HasKey(e => e.IdEspecialidad).HasName("PK__Especial__693FA0AF342E56B2");
+
+            entity.ToTable("Especialidad");
+
+            entity.Property(e => e.Nombre)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<Hospital>(entity =>
+        {
+            entity.HasKey(e => e.IdHospital).HasName("PK__Hospital__A80B2F2D15BE87AC");
+
+            entity.ToTable("Hospital");
+
+            entity.Property(e => e.AñoConstruccion).HasColumnType("date");
+            entity.Property(e => e.Direccion)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.Nombre)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+
+            entity.HasOne(d => d.IdEspecialidadNavigation).WithMany(p => p.Hospitals)
+                .HasForeignKey(d => d.IdEspecialidad)
+                .HasConstraintName("FK__Hospital__IdEspe__1273C1CD");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ PL/Controllers/HospitalController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PL.Controllers
+{
+    public class HospitalController : Controller
+    {
+        public IActionResult GetAllHospital()
+        {
+            Dictionary<string, object> resultado = BL.Hospital.GetAll();
+            bool correct = (bool)resultado["Resultado"];
+            if (correct)
+            {
+                ML.Hospital producto = (ML.Hospital)resultado["Hospital"];
+                return View(producto);
+            }
+            return View();
+        }
+
+        [HttpPost]
+
+        public ActionResult Form(ML.Hospital hospital)
+        {
+            ML.Hospital hosp = new ML.Hospital();
+            return View(hosp);
+        }
+
+
+
+
+
+
+        [HttpGet]
+        public ActionResult Delete(int idHospital)
+        {
+            Dictionary<string, object> result = BL.Hospital.Delete(idHospital);
+            bool resultado = (bool)result["Resultado"];
+
+            if (resultado == true)
+            {
+
+                ViewBag.Mensaje = "El Hospital ha sido eliminado";
+                return PartialView("Modal");
+
+            }
+            else
+            {
+                //pendiente una alerta para la excepcion
+                string excepcion = (string)result["Excepcion"];
+                ViewBag.Mensaje = "El Hospital no se ha podido eliminar";
+                return PartialView("Modal");
+
+            }
+        }
+    }
+}
